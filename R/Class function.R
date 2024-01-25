4a541fb35f8b4c524e084337d5c032e828c9f3da
ellencreed2023 <ellen.creed.2021@mumail.ie>
2024-01-25 22:38:26 +0000

    Updated version of class of results function

--- R/Class function.R
+++ R/Class function.R
@@ -1,34 +1,3 @@
-library(deSolve)
-library(tidyverse)
-library(devtools)
-#SIR model function
-sir_model <- function(time, state, parameters) {
-  with(as.list(c(state, parameters)), {
-    dS <- -beta * S * I
-    dI <- beta * S * I - gamma * I
-    dR <- gamma * I
-
-    return(list(c(dS, dI, dR)))
-  })
-}
-#parameters
-initial_state <- c(S = 0.99, I = 0.01, R = 0)
-parameters <- c(beta = 0.3, gamma = 0.1)
-
-times <- seq(0, 80, by = 1)
-
-#SIR results
-SIR_result <- function(initial_state, times, sir_model, parameters){
-  output <- ode(y = initial_state, times = times, func = sir_model, parms = parameters)
-  return(as.data.frame(output))
-}
-
-#Example of function
-SIR_result(initial_state, times, sir_model, parameters) {
-  output <- ode(y = initial_state, times = times, func = sir_model, parms = parameters)
-  return(as.data.frame(output))
-}
-
 #' SIR Model Class
 #'
 #' This class represents the Susceptible-Infectious-Recovered (SIR) model.
@@ -73,6 +42,8 @@ setMethod("initialize",
             .Object@parameters <- parameters
             return(.Object)
           })
+
+setGeneric("generate", function(object) standardGeneric("generate"))
 
 
 #Define a method to generate the SIR results
