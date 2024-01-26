a871a89045bf75f39b98a0292659329ab0665bdc
ellencreed2023 <ellen.creed.2021@mumail.ie>
2024-01-26 14:25:51 +0000

    authours added

--- R/Class_function.R
+++ R/Class_function.R
@@ -20,6 +20,7 @@
 #' @return An object of class SIRResult.
 #' @keywords methods initialize SIRResult
 #' @export
+#' @author "Joao Filipe" "Siun Mulcahy" "Ellen Creed"
 
 #' Generate SIR results
 #' @title Generates SIR model simulation results.
@@ -28,6 +29,7 @@
 #' @return An updated object of class SIRResult with the simulation results.
 #' @keywords methods generate SIRResult
 #' @export
+#' @author "Joao Filipe" "Siun Mulcahy" "Ellen Creed"
 #' @examples
 #' # Define initial state and parameters
 #' initial_state <- c(S = 0.99, I = 0.01, R = 0)
